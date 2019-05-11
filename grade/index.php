a0d0a4aff42cded3a6d5ce6046cf20b64be07e78
AS <xskachova@stuba.sk>
2019-05-11 09:44:21 +0200

    import z ulohy2

--- grade/index.php
+++ grade/index.php
@@ -47,6 +47,7 @@
                             <a class="nav-link" href="../sendlogininfo">Rozposielanie údajov</a>
                         </li>
                     </ul>
+                    
                     <div class="my-2 my-lg-0">
                         <div class="my-2 my-lg-0">
                             <?php if(isset($_SESSION['username'])) { ?>
@@ -74,10 +75,129 @@
             <section>
                 <h4>Hodnotenie</h4>
             </section>
-        </div>
-    </main>
+        
+            <!-- TU FUNKCIONALITA ><-->
+                    <div class="my-2 my-lg-0">
+                        <form enctype='multipart/form-data' action='index.php' method="POST">
+                            <label> Školský rok </label>
+                            <select name="year" required>
+                                <option value="1819">2018/2019</option>
+                                <option value="1718">2017/2018</option>
+                                <option value="1617">2016/2017</option>
+                                <option value="1516">2015/2016</option>
+                                <option value="1415">2014/2015</option>
+                            </select> <br>
+                            <label> Názov predmetu </label> <input type="text" name="subject" required> <br>
+                            <label> Vyberte súbor </label> <input type="file" name="userfile" accept=".csv" required /> <br>
+                            <label> Oddeľovač </label> 
+                                <input type="radio" name="delim" value="coma" required> čiarka 
+                                <input type="radio" name="delim" value="dotcoma" required> bodkočiarka <br>
+                            <input type='submit' name='submit' value='Testujeme' /> 
+                        </form>
+                    </div>
+
+        
+    <?php
+
+        // ako dat povinny parameter select?
+
+        include "../config.php";
     
-    <footer class="mt-4 pt-4 border-top">
+        if (isset($_POST['submit'])){
+             $conn = new mysqli($servername, $username, $password, $dbname);
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } else{
+                // echo "pripojene k db";
+            }
+            mysqli_set_charset($conn,"utf8");        
+            
+            $year = $_POST['year'];
+            $subject = $_POST['subject'];
+            $separator = $_POST['delim'];
+            $delim = "";
+
+            if($separator == "coma") $delim = ",";
+            if($separator == "dotcoma") $delim = ";";
+
+            echo "toto je delim ".$delim;
+
+            echo $subject.$year.$separator;
+
+            // pri kazdom vkladani udajov vymaz tabulku
+            mysqli_query($conn,'TRUNCATE TABLE student');
+
+            $filename = $_FILES['userfile']['name'];
+            // cela cesta k suboru
+            //$path = realpath($filename);
+            //echo $path;
+
+            // subor musi byt na servri
+            // https://stackoverflow.com/questions/2805427/how-to-extract-data-from-csv-file-in-php
+            // CITANIE S CSV    
+            if (($handle = fopen($filename, "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 1000, $delim)) !== FALSE) {
+                   $sql = "INSERT INTO student (id_student, meno, email, heslo, tim) VALUES ($data[0], '".$data[1]."', '".$data[2]."', '".$data[3]."', $data[4])";
+                   $result = mysqli_query($conn, $sql);
+
+                }
+                fclose($handle);
+            }
+            
+
+            // VYPISANIE DO TABULIEK
+            echo "<div class='my-2 my-lg-0'>";
+
+            $sql2 = "SELECT DISTINCT tim FROM student";
+            $result2 = mysqli_query($conn, $sql2);  
+            if (mysqli_num_rows($result2) > 0) {
+                while($row2 = mysqli_fetch_assoc($result2)) {
+
+                    echo "<h1> Clenovia timu ".$row2['tim']."</h1>";
+                    echo "<input type='number' id='body".$row2['tim']."'>";
+                    // ZMENA POMOCOU AJAX alebo XMLRPC!!!
+                    echo "<input type='submit' name='change' value='Change'>";
+
+
+                    $sql3 = "SELECT * FROM student WHERE tim=".$row2['tim'];
+                    $result3 = mysqli_query($conn, $sql3);  
+                    if (mysqli_num_rows($result3) > 0) {
+                        echo "<table>
+                            <thead><tr><th>ID</th>
+                                      <th>Meno</th>
+                                      <th>Email</th>
+                                      <th>Heslo</th>
+                                      <th>Tim</th>
+                                    </tr></thead><tbody>";
+
+                        while($row = mysqli_fetch_assoc($result3)) {
+                             echo "<tr><td>" . $row['id_student']."</td>
+                                       <td>" . $row['meno']."</td>
+                                       <td>" . $row['email']."</td>
+                                       <td>" . $row['heslo']."</td>
+                                       <td>" . $row['tim']."</td></tr>";        
+                         }
+                        
+                         echo "</tbody></table>";
+                         
+                    } else {
+                         echo "you have no records";
+                    }
+
+                }
+            } else {
+                 echo "you have no records";
+            }
+
+            echo "</div>";
+            
+        }
+     ?>
+
+        </div>
+       </main>
+
+     <footer class="mt-4 pt-4 border-top">
         <div class="container">
             <div class="row">
                 <div class="col-12 col-md">
@@ -102,5 +222,7 @@
         </div>
     </footer>
 </body>
+
+
 
 </html>
